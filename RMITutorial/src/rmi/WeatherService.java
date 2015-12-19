3fabaae47c39205756b8125cbe605c821fba63f3
elzbietaZ <elzbieta.zukrowska@gmail.com>
2015-12-19 20:48:02 +0100

    Example project

---
+++ RMITutorial/src/rmi/WeatherService.java
@@ -0,0 +1,13 @@
+package rmi;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.util.Date;
+ 
+ 
+public interface WeatherService extends Remote {
+  
+  // Method to retrieve weather information.
+  public WeatherData getWeather(Date date, String location)
+          throws RemoteException;
+}
